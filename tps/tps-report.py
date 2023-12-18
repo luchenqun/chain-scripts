b6089f93357c936f2ca84f166cd7f33d58dd3131
Kooler <kowpun123@gmail.com>
2023-12-18 17:28:04 +0700

    add save generated figure

--- tps/tps-report.py
+++ tps/tps-report.py
@@ -4,6 +4,10 @@ import sys
 
 
 def main(filename: str):
+    if not filename.endswith(".csv"):
+        print("Support only .csv file.")
+        exit(1)
+
     x = []
     y = []
 
@@ -21,16 +25,18 @@ def main(filename: str):
 
     plt.plot(x[1:], y[1:], color='g', linestyle='solid', label=filename)
 
-    plt.xlabel('Sent Transaction')
-    plt.ylabel('TPS')
+    plt.xlabel('Transaction Sent')
+    plt.ylabel('Calculated TPS')
     plt.xscale('log')
     plt.title(
         'TPS Report (4v CPU, 16 GB, 2 Validators, Block Time = 3s)', fontsize=12)
     plt.legend()
     plt.grid()
+    plt.savefig(filename.replace(".csv", ".png"))
     plt.show()
 
 
 if __name__ == "__main__":
+    print("The usage: python tps-report.py <file_generated_from_tps.js>.csv")
     filename = sys.argv[1]
     main(filename)
